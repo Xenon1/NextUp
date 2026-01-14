8b59c9a865ddbeffd04336176f813e3752e82364
Xenon1 <joecooke012@gmail.com>
2026-01-14 01:20:06 +0000

    0.2.10

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ import tmdbService from './services/tmdbService'
 import releaseService from './services/releaseService'
 import './App.css'
 
-const APP_VERSION = '0.2.9'
+const APP_VERSION = '0.2.10'
 type TabType = 'dashboard' | 'search' | 'movies' | 'tv' | 'anime'
 
 function App() {
